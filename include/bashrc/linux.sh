f3e7b14ebbe230d73edcdc48f32ac52683dffe23
Ondrej Brablc <ondrej@brablc.com>
2024-10-27 00:14:53 +0200

    Adapt to remote linux

--- include/bashrc/linux.sh
+++ include/bashrc/linux.sh
@@ -1,9 +1,12 @@
-export HOMEBREW_NO_AUTO_UPDATE=1
-export HOMEBREW_NO_INSTALL_CLEANUP=1
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+if [ -d /home/linuxbrew ]; then
+  export HOMEBREW_NO_AUTO_UPDATE=1
+  export HOMEBREW_NO_INSTALL_CLEANUP=1
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
 
 alias pbcopy='xclip -selection clipboard'
 alias pbpaste='xclip -selection clipboard -o'
+alias mc='source /usr/lib/mc/mc-wrapper.sh -d'
 
 source /usr/share/bash-completion/completions/fzf
 source /usr/share/doc/fzf/examples/key-bindings.bash
--- install/linux.sh
+++ install/linux.sh
@@ -1,1 +1,3 @@
+echo "Install starship manually: curl -sS https://starship.rs/install.sh | sh"
+
 sudo apt install -y fzf ripgrep bat eza zoxide plocate btop apache2-utils fd-find tldr
