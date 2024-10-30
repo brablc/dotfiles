eb6029422daa049d6a3170c2b6e954b9f40943ea
Ondrej Brablc <obrablc@purestorage.com>
2024-10-30 16:06:54 +0100

    Add neotest-python config

---
+++ home/.config/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = {
+      "nvim-neotest/neotest-python",
+    },
+    opts = {
+      adapters = {
+        ["neotest-python"] = {
+          runner = "pytest",
+          python = ".venv/bin/python",
+          dap = { justMyCode = false },
+        },
+      },
+    },
+  },
+}
