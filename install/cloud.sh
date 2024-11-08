8386c2d2981168494a1ffa0ecb6a0adbd4bbe4fe
Ondrej Brablc <obrablc@purestorage.com>
2024-11-08 16:01:11 +0100

    Add vault

--- install/cloud.sh
+++ install/cloud.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-brew install azure-cli kubernetes-cli node python@3.10 teleport terraform terramate
+brew tap hashicorp/tap
+brew install azure-cli kubernetes-cli node python@3.10 teleport terraform terramate hashicorp/tap/vault
 
 for COMMAND in az kubectl; do
   ln -s ${HOMEBREW_PREFIX:-/home/linuxbrew/.linuxbrew}/etc/bash_completion.d/$COMMAND ${HOME}/.local/share/bash-completion/completions/
