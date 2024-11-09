680fa40850c7a3e6d5b8fac1f6544326ec0f0631
Ondrej Brablc <obrablc@purestorage.com>
2024-11-09 19:06:13 +0100

    Add more mason goodies

---
+++ home/.config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "azure-pipelines-language-server",
+        "awk-language-server",
+        "bash-language-server",
+        "html-lsp",
+        "jq-lsp",
+        "jq",
+        "shellcheck",
+      },
+    },
+  },
+}
