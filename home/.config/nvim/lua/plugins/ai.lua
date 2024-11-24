351a776ada8cb21b8e738445e67f46fcc97db3d3
Ondrej Brablc <ondrej@brablc.com>
2024-11-24 22:26:27 +0100

    Add CodeCompletion with Anthropic support

---
+++ home/.config/nvim/lua/plugins/ai.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      -- The following are optional:
+      { "MeanderingProgrammer/render-markdown.nvim", ft = { "markdown", "codecompanion" } },
+    },
+    opts = {
+      strategies = {
+        chat = {
+          adapter = "anthropic",
+        },
+        inline = {
+          adapter = "copilot",
+        },
+      },
+      adapters = {
+        anthropic = function()
+          return require("codecompanion.adapters").extend("anthropic", {
+            env = {
+              api_key = "cmd:op read 'op://Private/Claude.ai Neovim API Key/credential' --no-newline",
+            },
+          })
+        end,
+      },
+    },
+  },
+}
