a66c5be4d6e8f62879348f1b86c0f4355939060a
Ondrej Brablc <obrablc@purestorage.com>
2024-11-28 16:36:22 +0100

    Add support for .tftpl

--- home/.config/nvim/lua/config/autocmds.lua
+++ home/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+vim.filetype.add({
+  extension = {
+    tftpl = function(path, bufnr)
+      local base = vim.fn.fnamemodify(path, ":r")
+      return vim.filetype.match({ filename = base })
+    end,
+  },
+})
