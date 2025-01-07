b435441afaf043cc5e11330be0a0f993ffbffd5c
Ondrej Brablc <obrablc@purestorage.com>
2025-01-07 14:50:06 +0100

    Extract shell mason

--- home/.config/nvim/lua/plugins/mason.lua
+++ home/.config/nvim/lua/plugins/mason.lua
@@ -5,12 +5,10 @@ return {
       ensure_installed = {
         -- "azure-pipelines-language-server",
         -- "awk-language-server",
-        "bash-language-server",
         "html-lsp",
         "phpcbf",
         -- "jq-lsp",
         -- "jq",
-        "shellcheck",
       },
     },
   },
---
+++ home/.config/nvim/lua/plugins/shell.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "bash-language-server",
+        "shellcheck",
+        "shfmt",
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        ["sh"] = { "shfmt" }, -- Add shfmt for shell scripts
+      },
+      formatters = {
+        shfmt = {
+          command = "shfmt",
+          args = { "-i", "2", "-ci", "-bn", "-simplify" }, -- Customize shfmt options
+          stdin = true, -- Ensure it uses stdin for formatting
+        },
+      },
+    },
+  },
+}
