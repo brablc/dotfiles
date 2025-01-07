c29d4c76b8ace84be9c8f5a99a3c5c77e3bf6f3f
Ondrej Brablc <obrablc@purestorage.com>
2025-01-07 14:50:57 +0100

    Add znd autocompletion

--- include/bashrc/zellij.sh
+++ include/bashrc/zellij.sh
@@ -12,4 +12,16 @@ function zpipe() {
   fi
 }
 function zpf() { zellij plugin -- filepicker; }
-function znt() { zellij action new-tab --layout "${1-half}" --cwd "$PWD" --name "${PWD##*/}"; }
+function znt() {
+  DIR=$(readlink -f ${1-$PWD})
+  zellij action new-tab --layout "${2-half}" --cwd "$DIR" --name "${DIR##*/}"
+}
+
+function _fzf_subdir_completion() {
+  __fzf_generic_path_completion _fzf_compgen_subdir "" "/" "$@"
+}
+function _fzf_compgen_subdir() {
+  command find -L "$1" -name .git -prune -o -name .hg -prune -o -name .svn -prune -o -type d -maxdepth 1 -a -not -path "$1" -print 2>/dev/null | command sed 's@^\./@@'
+}
+
+complete -o nospace -F _fzf_subdir_completion znt
