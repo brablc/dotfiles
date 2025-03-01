a501025b6b3595c85e02512eb74efeaea3f9fafa
Ondrej Brablc <obrablc@purestorage.com>
2025-03-01 13:27:52 +0100

    Improve augment

--- home/.config/nvim/lua/plugins/ai.lua
+++ home/.config/nvim/lua/plugins/ai.lua
@@ -35,8 +35,16 @@ return {
       vim.keymap.set("n", "<leader>al", function()
         vim.cmd("Augment chat " .. vim.api.nvim_get_current_line())
       end, { desc = "Augment chat with current line" })
+      vim.keymap.set("n", "<leader>ap", function()
+        local clipboard = vim.fn.getreg("+")
+        clipboard = clipboard:gsub("\\", "\\\\"):gsub("\n", "\\n")
+        local prompt = vim.fn.input("Enter prompt for clipboard")
+        if prompt ~= "" then
+          vim.cmd("Augment chat " .. prompt .. ":\\n" .. clipboard)
+        end
+      end, { desc = "Augment chat with prompt and clipboard" })
       vim.keymap.set({ "n", "v" }, "<leader>ac", "<cmd>Augment chat<CR>")
-      vim.keymap.set("n", "<leader>at", "<cmd>Augment chat-toggle<CtR>")
+      vim.keymap.set({ "n", "v" }, "<leader>at", "<cmd>Augment chat-toggle<CR>")
       vim.keymap.set("i", "<C-a>", "<cmd>call augment#Accept()<CR>", { silent = true })
     end,
   },
