ed808d1b775bd2106ef2deb62866beaa0bb61bf4
Ondrej Brablc <ondrej@brablc.com>
2025-03-22 15:49:54 +0100

    Upgrade zellij

--- install/macos.sh
+++ install/macos.sh
@@ -1,1 +1,7 @@
 brew install starship luarocks yq
+
+mkdir -p ~/.local/share/bash-completion/completions
+ln -s ~/.local/share/bash-completion ~/.config
+
+curl -Ls https://github.com/zellij-org/zellij/releases/latest/download/zellij-x86_64-apple-darwin.tar.gz | tar xzf - && mv -v zellij ~/.local/bin
+zellij setup --generate-completion bash >~/.local/share/bash-completion/completions/zellij
