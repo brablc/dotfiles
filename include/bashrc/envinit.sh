2cbfa328bd5b647596e1a0f270ccad00850a257c
Ondrej Brablc <obrablc@purestorage.com>
2025-04-01 16:27:53 +0200

    Change to act as direnv

--- include/bashrc/envinit.sh
+++ include/bashrc/envinit.sh
@@ -1,11 +1,17 @@
 # shellcheck disable=SC1090,SC1091
 
-ENVINIT_DIR=$HOME/.config/envinit${PWD//$HOME/}
-ENVINIT_SOURCE_FILE="$ENVINIT_DIR/source.sh"
+function envinit() {
+  ENVINIT_DIR=$HOME/.config/envinit${PWD//$HOME/}
+  ENVINIT_SOURCE_FILE="$ENVINIT_DIR/source.sh"
 
-if [[ -f $ENVINIT_SOURCE_FILE ]]; then
-  source "$ENVINIT_SOURCE_FILE"
-elif [[ -f ./env_setup.sh ]]; then
-  ENVINIT_DIR=$PWD
-  source ./env_setup.sh
-fi
+  if [[ -f $ENVINIT_SOURCE_FILE ]]; then
+    source "$ENVINIT_SOURCE_FILE"
+  elif [[ -f ./env_setup.sh ]]; then
+    ENVINIT_DIR=$PWD
+    source ./env_setup.sh
+  elif [[ -f ./.venv ]]; then
+    source ./.venv/bin/activate
+  fi
+}
+
+PROMPT_COMMAND="envinit; $PROMPT_COMMAND"
