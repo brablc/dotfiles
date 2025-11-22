48492e78feb0fc7720427aff7ebc0eea432b2a1f
Ondrej Brablc <obrablc@purestorage.com>
2025-11-23 00:03:03 +0100

    Install lazydocker

--- install/common.sh
+++ install/common.sh
@@ -1,3 +1,5 @@
 install -m 755 /dev/stdin "$HOME/.local/bin/h" < <(curl -s -L https://raw.githubusercontent.com/paoloantinori/hhighlighter/master/h.sh\?safe)
 
 echo 'h "$@"' >>"$HOME/.local/bin/h"
+
+brew install lazydocker
